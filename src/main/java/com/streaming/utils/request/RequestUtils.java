f7f9735c86e64776e9486a62d419974b8093a9dc
nischal1 <nischal@customfurnish.com>
2020-07-14 13:37:52 +0530

    bad request exception handling

--- src/main/java/com/streaming/utils/request/RequestUtils.java
+++ src/main/java/com/streaming/utils/request/RequestUtils.java
@@ -58,7 +58,7 @@ public final class RequestUtils {
                     sb.append(output);
                 }
 
-                throw new RequestException("403 Exception " + br.toString());
+                throw new RequestException("403 Exception " + sb.toString());
             } else if (connection.getResponseCode() == HttpStatus.BAD_REQUEST.value()) {
                 br = new BufferedReader(new InputStreamReader((connection.getErrorStream())));
                 String output;
@@ -67,7 +67,7 @@ public final class RequestUtils {
                     sb.append(output);
                 }
 
-                throw new RequestException("400 Exception " + br.toString());
+                throw new RequestException("400 Exception " + sb.toString());
             }
 
         } catch (IOException e) {
