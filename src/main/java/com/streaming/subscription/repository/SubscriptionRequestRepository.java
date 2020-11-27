d93a5dc5cff08d5edbe566f11d89cc738dcb3b38
athmakoor <Swapnika09>
2020-11-27 12:39:30 +0530

    pack price logic for zain kuwait

--- src/main/java/com/streaming/subscription/repository/SubscriptionRequestRepository.java
+++ src/main/java/com/streaming/subscription/repository/SubscriptionRequestRepository.java
@@ -1,5 +1,6 @@
 package com.streaming.subscription.repository;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -8,4 +9,6 @@ import com.streaming.subscription.bean.jpa.SubscriptionRequestEntity;
 
 public interface SubscriptionRequestRepository extends PagingAndSortingRepository<SubscriptionRequestEntity, Integer> {
     List<SubscriptionRequestEntity> findByClickId(String clickId);
+
+    List<SubscriptionRequestEntity> findByCreatedAtAfter(ZonedDateTime date);
 }
--- src/main/java/com/streaming/subscription/service/impl/SubscriptionServiceImpl.java
+++ src/main/java/com/streaming/subscription/service/impl/SubscriptionServiceImpl.java
@@ -1,9 +1,12 @@
 package com.streaming.subscription.service.impl;
 
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import javax.annotation.Resource;
 
+import com.streaming.constant.Provider;
 import org.springframework.stereotype.Service;
 
 import com.streaming.exception.ConstraintViolationException;
@@ -34,6 +37,7 @@ public class SubscriptionServiceImpl implements SubscriptionService {
             throw new ConstraintViolationException("Duplicate click id");
         }
 
+        updatePack(data);
         GenerateOTPResponse response = SubscriptionUtils.generateOTP(data);
 
         SubscriptionRequestEntity entity = new SubscriptionRequestEntity();
@@ -46,6 +50,21 @@ public class SubscriptionServiceImpl implements SubscriptionService {
 
         subscriptionRequestRepository.save(entity);
         return response;
+    }
+
+    private void updatePack(GenerateOTPRequest data) {
+        ZonedDateTime date = TimeUtil.getCurrentUTCTime().truncatedTo(ChronoUnit.DAYS);
+        List<SubscriptionRequestEntity> entities = subscriptionRequestRepository.findByCreatedAtAfter(date);
+
+        if (data.getProvider().equals(Provider.ZAIN_KUWAIT)) {
+            if (entities.size() > 30) {
+                data.setPackPrice("100");
+                data.setPackValidity("1");
+            } else {
+                data.setPackPrice("600");
+                data.setPackValidity("7");
+            }
+        }
     }
 
     @Override
@@ -65,6 +84,7 @@ public class SubscriptionServiceImpl implements SubscriptionService {
 
     @Override
     public GenerateOTPResponse regenerateOTP(GenerateOTPRequest data) {
+        updatePack(data);
         GenerateOTPResponse response = SubscriptionUtils.regenerateOTP(data);
 
         SubscriptionRequestEntity entity = new SubscriptionRequestEntity();
